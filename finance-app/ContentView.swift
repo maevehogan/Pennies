006394097d605ab7a59d03942e1ae27235219a47
maevehogan <maevejhogan@icloud.com>
2026-01-03 20:38:26 -0500

    Initial Commit

---
+++ finance-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  finance-app
+//
+//  Created by Maeve Hogan on 1/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ finance-app/finance_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  finance_appApp.swift
+//  finance-app
+//
+//  Created by Maeve Hogan on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct finance_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
